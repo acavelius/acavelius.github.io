7a679b47bae59e3a32aa444a39da03902bd6dcc4
acavelius <46372348+acavelius@users.noreply.github.com>
2019-05-17 18:22:34 +0200

    sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@@Email.com" );
+define( "RECIPIENT_NAME", "Laurent CAVELIUS" );
+define( "RECIPIENT_EMAIL", "contact@smart-energeia.com" );
 
 
 // Read the form values
