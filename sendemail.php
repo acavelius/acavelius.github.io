80d04638a2a35fc89eea8baad6214e940d2f3854
acavelius <46372348+acavelius@users.noreply.github.com>
2019-12-10 14:57:18 +0100

    Add files via upload

--- sendemail.php
+++ sendemail.php
@@ -7,9 +7,9 @@ define( "RECIPIENT_EMAIL", "contact@smart-energeia.com" );
 
 // Read the form values
 $success = false;
-$userName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
+$userName = isset( $_POST['nom'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['nom'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$senderSubject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+$senderSubject = isset( $_POST['societe'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['societe'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
